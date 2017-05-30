7a650aa398320697921d34f687957a9d53a4d9c6
Phuong Do <dongphuong2410@yahoo.com>
2017-05-30 16:54:32 +0900

    Finish refactoring code for policy, add one more testcase

--- tests/test002.c
+++ tests/test002.c
@@ -9,6 +9,7 @@ void _print_policy(policy_t *policy);
 
 int main(int argc, char *argv)
 {
+    //log_init(LV_DEBUG, LOG_CONSOLE);
     const char *policy_file = "test002.pol";
     GSList *list = get_policies(policy_file);
     printf("%d\n", g_slist_length(list));
