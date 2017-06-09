ee5815156323bc2d6c60813d3bf07c761ba120bf
Phuong Do <dongphuong2410@yahoo.com>
2017-06-09 14:20:14 +0900

    Call vmi_slat_destroy for the view created

--- xen_helper.c
+++ xen_helper.c
@@ -44,21 +44,6 @@ void xen_free_interface(xen_interface_t *xen)
     }
 }
 
-int xen_enable_altp2m(xen_interface_t *xen)
-{
-    return xc_altp2m_set_domain_state(xen->xc, xen->domID, 1);
-}
-
-int xen_create_view(xen_interface_t *xen, uint16_t *idx)
-{
-    return xc_altp2m_create_view(xen->xc, xen->domID, 0, idx);
-}
-
-int xen_switch_view(xen_interface_t *xen, uint16_t idx)
-{
-    return xc_altp2m_switch_to_view(xen->xc, xen->domID, idx);
-}
-
 addr_t xen_extend_extra_frame(xen_interface_t *xen, uint64_t proposed_memsize)
 {
     int rc;
