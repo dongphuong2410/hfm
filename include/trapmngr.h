54ac3d594c2c2f66a7cde5b265b2d7e7cd945ed6
Author Phuong Do <dongphuong2004@gmail.com>
2017-06-14 10:20:49 +0900

    Refactor : rename traps.c file to trapmngr.c file

---
+++ include/trapmngr.h
@@ -0,0 +1,66 @@
+/**
+  * @file trapmngr.h
+  * @author phuong.do
+  * @date 2017-06-13
+  * @brief Managing all the information about traps in the system
+  */
+#ifndef __TRAPS_H__
+#define __TRAPS_H__
+
+#include "private.h"
+
+/**
+  * @brief Contain list of int3 breakpoints set at same position
+  */
+typedef struct int3_wrapper_t {
+    GSList *traplist;
+} int3_wrapper_t;
+
+/**
+  * @brief Contain a mapping address pair
+  */
+typedef struct _remapped_t {
+    uint64_t o;     //Original address
+    uint64_t r;     //Remapped address
+} remapped_t;
+
+/**
+  * @brief A trap to be injected to the VM
+  */
+typedef struct trap_t {
+    char name[STR_BUFF];
+} trap_t;
+
+/**
+  * @brief Init trap manager
+  * @param vm Pointer to vmhdlr_t struct
+  * @return Return pointer to trapmngr_t if init success, or else return NULL
+  */
+trapmngr_t *trapmngr_init(vmhdlr_t *vm);
+
+/**
+  * @brief Destroy trap manager, releases all resources
+  * @param Pointer to trapmngr_t
+  */
+void trapmngr_destroy(trapmngr_t *trapmngr);
+
+/**
+  * @brief Add a remapped page to remapped hashtable
+  * @param trapmngr Pointer to trapmng_t
+  * @param original Original gfn
+  * @param remapped Remapped gfn
+  */
+void trapmngr_add_remapped(trapmngr_t *trapmngr, uint64_t original, uint64_t remapped);
+
+/**
+  * @brief Find a remapped page of a frame
+  * @param trapmngr Pointer to trapmngr
+  * @param origin Original frame number
+  * @return Return address of remap of this frame, or return 0 if not exist
+  */
+uint64_t trapmngr_find_remapped(trapmngr_t *trapmngr, uint64_t original);
+
+int3_wrapper_t *trapmngr_find_breakpoint(trapmngr_t *trapmngr, uint64_t pa);
+
+void trapmngr_add_breakpoint(trapmngr_t *trapmngr, uint64_t *pa, int3_wrapper_t *wrapper);
+#endif
--- trapmngr.c
+++ trapmngr.c
@@ -7,30 +7,25 @@
 
 #include "private.h"
 #include "log.h"
-#include "traps.h"
+#include "trapmngr.h"
 #include "xen_helper.h"
 
 struct _trapmngr_t {
     vmhdlr_t *handler;
-    GHashTable *remapped_tbl;
-    GHashTable *breakpoint_tbl;
+    GHashTable *remapped_tbl;       /* Key : original frame no, Value : remapped_pair_t */
+    GHashTable *breakpoint_tbl;     /* Key : original frame, Value : remapped_t */
 };
 
-typedef struct _remapped_t {
-    uint64_t o;     //Original address
-    uint64_t r;     //Remapped address
-} remapped_t;
-
-trapmngr_t *traps_init(vmhdlr_t *vm)
+trapmngr_t *trapmngr_init(vmhdlr_t *vm)
 {
-    trapmngr_t *traps = (trapmngr_t *)calloc(1, sizeof(trapmngr_t));
-    traps->handler = vm;
-    traps->remapped_tbl = g_hash_table_new_full(g_int64_hash, g_int64_equal, NULL, free);
-    traps->breakpoint_tbl =g_hash_table_new_full(g_int64_hash, g_int64_equal, free, free);
-    return traps;
+    trapmngr_t *trapmngr = (trapmngr_t *)calloc(1, sizeof(trapmngr_t));
+    trapmngr->handler = vm;
+    trapmngr->remapped_tbl = g_hash_table_new_full(g_int64_hash, g_int64_equal, NULL, free);
+    trapmngr->breakpoint_tbl =g_hash_table_new_full(g_int64_hash, g_int64_equal, free, free);
+    return trapmngr;
 }
 
-void traps_add_remapped(trapmngr_t *trap_manager, uint64_t original, uint64_t remapped)
+void trapmngr_add_remapped(trapmngr_t *trap_manager, uint64_t original, uint64_t remapped)
 {
     remapped_t *r = (remapped_t *)calloc(1, sizeof(remapped_t));
     r->o = original;
@@ -38,7 +33,7 @@ void traps_add_remapped(trapmngr_t *trap_manager, uint64_t original, uint64_t re
     g_hash_table_insert(trap_manager->remapped_tbl, &original, r);
 }
 
-uint64_t traps_find_remapped(trapmngr_t *trap_manager, uint64_t original)
+uint64_t trapmngr_find_remapped(trapmngr_t *trap_manager, uint64_t original)
 {
     remapped_t *r = g_hash_table_lookup(trap_manager->remapped_tbl, &original);
     if (r)
@@ -46,7 +41,7 @@ uint64_t traps_find_remapped(trapmngr_t *trap_manager, uint64_t original)
     return 0;
 }
 
-void traps_destroy(trapmngr_t *trap_manager)
+void trapmngr_destroy(trapmngr_t *trap_manager)
 {
     //Free remapped_tbl
     GHashTableIter i;
@@ -64,12 +59,12 @@ void traps_destroy(trapmngr_t *trap_manager)
     free(trap_manager);
 }
 
-int3break_t *traps_find_breakpoint(trapmngr_t *trap_manager, uint64_t pa)
+int3_wrapper_t *trapmngr_find_breakpoint(trapmngr_t *trap_manager, uint64_t pa)
 {
     return g_hash_table_lookup(trap_manager->breakpoint_tbl, &pa);
 }
 
-void traps_add_breakpoint(trapmngr_t *trap_manager, uint64_t *pa, int3break_t *wrapper)
+void trapmngr_add_breakpoint(trapmngr_t *trap_manager, uint64_t *pa, int3_wrapper_t *wrapper)
 {
     g_hash_table_insert(trap_manager->breakpoint_tbl, pa, wrapper);
 }
