341b5f090274376c02cfc224d4780e371208c6ca
Author Phuong Do <dongphuong2004@gmail.com>
2017-06-28 09:18:54 +0900

    Add init code to file_created monitor plugin

--- monitors/file_created.c
+++ monitors/file_created.c
@@ -1,7 +1,24 @@
 #include "file_created.h"
+#include "hfm.h"
+#include "log.h"
 
+int syscall_cb(vmhdlr_t *handler, trap_data_t *data);
+int sysret_cb(vmhdlr_t *handler, trap_data_t *data);
 
 hfm_status_t file_created_add_policy(vmhdlr_t *hdlr, policy_t *policy)
 {
+    hfm_monitor_syscall(hdlr, "NtCreateFile", syscall_cb, sysret_cb);
     return FAIL;
+}
+
+int syscall_cb(vmhdlr_t *handler, trap_data_t *data)
+{
+    writelog(LV_DEBUG, "NtCreateFile start");
+    return 1;
+}
+
+int sysret_cb(vmhdlr_t *handler, trap_data_t *data)
+{
+    writelog(LV_DEBUG, "NtCreateFile return");
+    return 0;
 }
--- monitors/monitor.c
+++ monitors/monitor.c
@@ -1,32 +1,34 @@
+#include "file_created.h"
 #include "libmon.h"
-#include "hfm.h"
 #include "private.h"
-#include "log.h"
 
 
 void mon_init(monitor_t type)
 {
 }
 
-int syscall_cb(vmhdlr_t *handler, trap_data_t *data);
-int sysret_cb(vmhdlr_t *handler, trap_data_t *data);
-
 hfm_status_t mon_add_policy(vmhdlr_t *hdlr, policy_t *policy)
 {
-    hfm_monitor_syscall(hdlr, "NtCreateFile", syscall_cb, sysret_cb);
-    //hfm_monitor_syscall(hdlr, "NtCreateFile", cb_create, NULL);
-    //hfm_monitor_syscall(hdlr, "NtOpenFile", cb_open, NULL);
+    switch (policy->type) {
+        case MON_CREATE:
+            file_created_add_policy(hdlr, policy);
+            break;
+        case MON_DELETE:
+            break;
+        case MON_MODIFY_CONTENT:
+            break;
+        case MON_MODIFY_LOGFILE:
+            break;
+        case MON_CHANGE_ATTR_READONLY:
+            break;
+        case MON_CHANGE_ATTR_PERMISSION:
+            break;
+        case MON_CHANGE_ATTR_OWNERSHIP:
+            break;
+        case MON_CHANGE_ATTR_HIDDEN:
+            break;
+        default:
+            break;
+    }
     return SUCCESS;
-}
-
-int syscall_cb(vmhdlr_t *handler, trap_data_t *data)
-{
-    writelog(LV_DEBUG, "NtCreateFile start");
-    return 1;
-}
-
-int sysret_cb(vmhdlr_t *handler, trap_data_t *data)
-{
-    writelog(LV_DEBUG, "NtCreateFile return");
-    return 0;
 }
