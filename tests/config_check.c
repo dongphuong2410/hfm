9f2dfc33793e0258a3664e6c9d6a3cc2f2f6f2dc
Phuong Do <dongphuong2004@gmail.com>
2017-12-06 13:10:12 +0900

    Finish unittest for config module

--- tests/config_check.c
+++ tests/config_check.c
@@ -1,15 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <check.h>
+#include <unistd.h>
 #include "config.h"
 
 START_TEST(test_config_module)
+{
     int error = 1;
     char buff[1024];
     char cfgpath[1024];
-    char *cwd = getcwd(buff, 1024 + 1);
+    char *cwd = getcwd(buff, 1024);
     sprintf(cfgpath, "%s/%s", cwd, "sample_config.cfg");
-    ck_assert(error);
+    config_t *config = config_init(cfgpath);
+    int age = config_get_int(config, (const char *)"age");
+    ck_assert_int_eq(config_get_int(config, (const char *)"age"), 10);
+    ck_assert_str_eq(config_get_str(config, (const char *)"name"), "DongPhuong");
+    ck_assert_int_eq(config_get_int(config, (const char *)"salary"), -1);
+    ck_assert_ptr_null(config_get_str(config, (const char *)"school"));
+}
 END_TEST
 
 static Suite *config_suite(void)
@@ -17,7 +25,8 @@ static Suite *config_suite(void)
     Suite *s;
     TCase *tc_core;
 
-    s = suite_create("Config Module");
+    s = suite_create("Config");
+    tc_core = tcase_create("Core");
     tcase_add_test(tc_core, test_config_module);
     suite_add_tcase(s, tc_core);
     return s;
