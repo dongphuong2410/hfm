75f70d799d397725b0b44baccb7341ae1016e8be
Phuong Do <dongphuong2004@gmail.com>
2017-12-06 14:57:32 +0900

    Finish unit test for log.c file

--- tests/log_check.c
+++ tests/log_check.c
@@ -4,8 +4,115 @@
 #include <unistd.h>
 #include "log.h"
 
-START_TEST(test_log_level)
+START_TEST(test_log_level_debug)
 {
+    const char *log_file = "hfm.log";
+    remove(log_file);
+    log_init(LV_DEBUG, LOG_TEXTFILE, ".");
+    writelog(0, LV_DEBUG, "%s", "debug level - debug log");
+    writelog(0, LV_WARN, "%s", "debug level - warn log");
+    writelog(0, LV_FATAL, "%s", "debug level - fatal log");
+    log_close();
+
+    FILE *fp = fopen(log_file, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    ssize_t read;
+    size_t len = 0;
+    char *line = NULL;
+    int contains;
+    read = getline(&line, &len, fp);
+    ck_assert_int_ne(read, -1);
+    contains = strstr(line, "debug level - debug log") != NULL;
+    ck_assert(contains);
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_ne(read, -1);
+    contains = strstr(line, "debug level - warn log") != NULL;
+    ck_assert(contains);
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_ne(read, -1);
+    contains = strstr(line, "debug level - fatal log") != NULL;
+    ck_assert(contains);
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_eq(read, -1);
+
+    fclose(fp);
+
+    remove(log_file);
+}
+END_TEST
+
+START_TEST(test_log_level_warn)
+{
+    const char *log_file = "hfm.log";
+    remove(log_file);
+    log_init(LV_WARN, LOG_TEXTFILE, ".");
+    writelog(0, LV_INFO, "%s", "warn level - info log");    //Not logged
+    writelog(0, LV_WARN, "%s", "warn level - warn log");    //Logged
+    writelog(0, LV_ERROR, "%s", "warn level - error log");  //Logged
+    log_close();
+
+    FILE *fp = fopen(log_file, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    ssize_t read;
+    size_t len = 0;
+    char *line = NULL;
+    int contains;
+    read = getline(&line, &len, fp);
+    ck_assert_int_ne(read, -1);
+    contains = strstr(line, "warn level - warn log") != NULL;
+    ck_assert(contains);
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_ne(read, -1);
+    contains = strstr(line, "warn level - error log") != NULL;
+    ck_assert(contains);
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_eq(read, -1);
+
+    fclose(fp);
+
+    remove(log_file);
+}
+END_TEST
+
+START_TEST(test_log_level_fatal)
+{
+    const char *log_file = "hfm.log";
+    remove(log_file);
+    log_init(LV_FATAL, LOG_TEXTFILE, ".");
+    writelog(0, LV_INFO, "%s", "fatal level - info log");    //Not logged
+    writelog(0, LV_WARN, "%s", "fatal level - warn log");    //Not logged
+    writelog(0, LV_FATAL, "%s", "fatal level - fatal log");  //Logged
+    log_close();
+
+    FILE *fp = fopen(log_file, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    ssize_t read;
+    size_t len = 0;
+    char *line = NULL;
+    int contains;
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_ne(read, -1);
+    contains = strstr(line, "fatal level - fatal log") != NULL;
+    ck_assert(contains);
+
+    read = getline(&line, &len, fp);
+    ck_assert_int_eq(read, -1);
+
+    fclose(fp);
+
+    remove(log_file);
 }
 END_TEST
 
@@ -16,7 +123,9 @@ static Suite *log_suite(void)
 
     s = suite_create("Config");
     tc_core = tcase_create("Core");
-    tcase_add_test(tc_core, test_log_level);
+    tcase_add_test(tc_core, test_log_level_debug);
+    tcase_add_test(tc_core, test_log_level_warn);
+    tcase_add_test(tc_core, test_log_level_fatal);
     suite_add_tcase(s, tc_core);
     return s;
 }
